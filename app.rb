ae6342a5615f2b2ed295d8e733c7fcce3acb29e8
kleer <kleer@kleer-csd-box>
2018-05-25 11:21:00 -0500

    HangMiles pantalla inicial

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
 require './config'
+require './lib/HangMiles.rb'
 
 get '/' do
-
+	session["juego"] =HangMiles.new "india"
+	session["mensaje"]=session["juego"].NumCaracter
+	erb :index 
 end
+
+#post '/adivinar' do
+	  
+#end
+
+#post '/jugar' do 
+#	session["juego"] =  PicasYFijas.new(params['secreto'])
+#	session["mensaje"] = session["juego"].validarSecreto
+#	erb :juego
+#end
+
+#post '/adivinar' do
+#	session['resultado'] = "#{params['opcion']} 0f 0p" 
+#	erb :juego
+#end
---
+++ lib/HangMiles.rb
@@ -0,0 +1,12 @@
+class HangMiles
+#	def initialize secreto
+#		@secreto = secreto
+#	end
+#	def validarSecreto
+#		if @secreto.length !=4 or @secreto.to_i.to_s.split('').uniq.length != 4
+#			"El numero no es valido"
+#		else
+#			"El numero fue registrado correctamente"
+#		end	
+#	end
+end
